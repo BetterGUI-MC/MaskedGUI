af2e6cf59309e5f93fc5faed5da8dbd7e924673f
HSGamer <huynhqtienvtag@gmail.com>
2023-01-13 19:17:58 +0700

    don't have to init buttons

--- src/main/java/me/hsgamer/bettergui/maskedgui/util/MaskUtil.java
+++ src/main/java/me/hsgamer/bettergui/maskedgui/util/MaskUtil.java
@@ -16,29 +16,20 @@ public final class MaskUtil {
         // EMPTY
     }
 
-    public static Map<String, WrappedButton> createButtons(WrappedMask wrappedMask, Map<String, Object> buttonMap, String prefix, boolean initButton) {
+    public static Map<String, WrappedButton> createButtons(WrappedMask wrappedMask, Map<String, Object> buttonMap, String prefix) {
         Map<String, WrappedButton> buttons = new LinkedHashMap<>();
         for (Map.Entry<String, Object> entry : buttonMap.entrySet()) {
             String name = entry.getKey();
             Optional<Map<String, Object>> optionalValues = MapUtil.castOptionalStringObjectMap(entry.getValue());
             if (!optionalValues.isPresent()) continue;
             Map<String, Object> values = new CaseInsensitiveStringMap<>(optionalValues.get());
-            ButtonBuilder.INSTANCE.build(new ButtonBuilder.Input(wrappedMask.getMenu(), prefix + name, values)).ifPresent(button -> {
-                if (initButton) {
-                    button.init();
-                }
-                buttons.put(name, button);
-            });
+            ButtonBuilder.INSTANCE.build(new ButtonBuilder.Input(wrappedMask.getMenu(), prefix + name, values)).ifPresent(button -> buttons.put(name, button));
         }
         return buttons;
     }
 
-    public static Map<String, WrappedButton> createButtons(WrappedMask wrappedMask, Map<String, Object> buttonMap, boolean initButton) {
-        return createButtons(wrappedMask, buttonMap, wrappedMask.getName() + "_button_", initButton);
-    }
-
     public static Map<String, WrappedButton> createButtons(WrappedMask wrappedMask, Map<String, Object> buttonMap) {
-        return createButtons(wrappedMask, buttonMap, true);
+        return createButtons(wrappedMask, buttonMap, wrappedMask.getName() + "_button_");
     }
 
     public static void refreshButtons(UUID uuid, Collection<Button> buttons) {

f18b7bda2a27d54200db0cbcdac8225159a7b05f
HSGamer <huynhqtienvtag@gmail.com>
2023-01-15 09:44:13 +0700

    cancel when maxed

--- src/main/java/me/hsgamer/bettergui/maskedgui/mask/SequenceMask.java
+++ src/main/java/me/hsgamer/bettergui/maskedgui/mask/SequenceMask.java
@@ -148,6 +148,7 @@ public class SequenceMask implements WrappedMask {
                 index++;
             } else {
                 maxed = true;
+                cancel();
             }
         }
 
